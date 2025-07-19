2fba6ad30d7d009c8cbc2e7520e5df9e86254432
raulbellosom <raulbellosom@users.noreply.github.com>
2025-07-19 22:12:49 +0000

    deploy: 33bf0551611560ce6c2bd5e0d4e3a7d9a228ad2e

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ntropia/sw.js', { scope: '/ntropia/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
